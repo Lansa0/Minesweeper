a9daf4222e5475543c878bda310666e301083be6
Lansa0 <jysn.navea@gmail.com>
2025-06-01 22:38:34 -0400

    more inputs

--- main.cpp
+++ main.cpp
@@ -1,20 +1,56 @@
 #include "output.hpp"
 #include "input.hpp"
-
-
-std::string F = "ABCD";
+#include "tiles.hpp"
 
 // Set to 85x34
-// clang++ -std=c++11 -IHeaders -o Binary/minesweeper main.cpp Sources/output.cpp Sources/input.cpp && ./Binary/minesweeper
+// clang++ -std=c++11 -IHeaders -o binary/minesweeper main.cpp Sources/output.cpp Sources/input.cpp Sources/tiles.cpp && ./binary/minesweeper
 
-int main() 
-{
+int main() {
     Output::Dashboard();
+
+    bool NewBoard = true;
 
     do
     {
         const Input::Response Response = Input::Get();
-        Output::Log(Response.Key,Response.Tile);
+
+        bool FailedInput = false;
+        switch (Response.Key) {
+            case 'T': {
+                if (NewBoard) {
+                    NewBoard = false;
+                    Tiles::generateBoard(Response.Tile);
+                }
+
+                Tiles::TapResponse TR = Tiles::Tap(Response.Tile);
+                Output::Reveal(TR.Tiles);
+
+                break;
+            }
+
+            case 'F':
+                if (NewBoard) {
+                    FailedInput = true;
+                    break;
+                }
+
+            case 'U':
+                if (NewBoard) {
+                    FailedInput = true;
+                    break;
+                }
+
+            case 'R':
+                if (NewBoard) {
+                    FailedInput = true;
+                    break;
+                }
+                // Reset Board
+
+            default: break;
+        }
+
+        Output::Log(Response.Key,Response.Tile,FailedInput);
 
     } while (true);
 
