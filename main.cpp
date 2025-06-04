8ee84095633478b86b539af32e8127ce6a7696b6
Lansa0 <jysn.navea@gmail.com>
2025-06-04 13:07:08 -0400

    burp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "responses.hpp"
 #include "output.hpp"
 #include "input.hpp"
 #include "tiles.hpp"
@@ -5,52 +6,97 @@
 // Set to 85x34
 // clang++ -std=c++11 -IHeaders -o binary/minesweeper main.cpp Sources/output.cpp Sources/input.cpp Sources/tiles.cpp && ./binary/minesweeper
 
+// TODO
+// recreate and fix that one bug, somehow
+
 int main() {
     Output::Dashboard();
 
     bool NewBoard = true;
 
-    do
-    {
-        const Input::Response Response = Input::Get();
+    States::Game GameState;
 
-        bool FailedInput = false;
-        switch (Response.Key) {
-            case 'T': {
+    do {
+        const Responses::Input Response = Input::Get();
+        const auto Tile = Response.Tile;
+        const auto Key = Response.Key;
+
+        switch (Key) {
+            case States::Input::Tap: {
                 if (NewBoard) {
                     NewBoard = false;
-                    Tiles::generateBoard(Response.Tile);
+                    Tiles::generateBoard(Tile);
                 }
 
-                Tiles::TapResponse TR = Tiles::Tap(Response.Tile);
-                Output::Reveal(TR.Tiles);
+                const Responses::Tap TR = Tiles::Tap(Tile);
+                GameState = TR.State;
 
+                Output::Reveal(TR);
+                Output::Log(States::Log::Tap,Tile);
                 break;
             }
 
-            case 'F':
+            case States::Input::Flag: {
                 if (NewBoard) {
-                    FailedInput = true;
+                    Output::Log(States::Log::Failed,Tile);
                     break;
                 }
 
-            case 'U':
-                if (NewBoard) {
-                    FailedInput = true;
-                    break;
+                const Responses::Flag FR = Tiles::Flag(Tile);
+
+                States::Log LogState;
+                if (FR.State == States::Flag::Add) {
+                    LogState = States::Log::Flag;
+                } else if (FR.State == States::Flag::Remove) {
+                    LogState = States::Log::Unflag;
+                } else {
+                    LogState = States::Log::Flagfail;
                 }
 
-            case 'R':
+                Output::Flag(Tile,FR);
+                Output::Log(LogState,Tile);
+                break;
+            }
+
+            case States::Input::Reset: {
                 if (NewBoard) {
-                    FailedInput = true;
+                    Output::Log(States::Log::Failed,Tile);
                     break;
                 }
-                // Reset Board
+                NewBoard = true;
 
-            default: break;
+                Output::Reset();
+                Output::Log(States::Log::Reset,Tile);
+                break;
+            }
+
+            case States::Input::Bad: {
+                Output::Log(States::Log::Bad,Tile);
+                break;
+            }
+
+            case States::Input::Quit: {
+                Output::Log(States::Log::Quit,Tile);
+                // Add a proper exit
+                return 1;
+            }
         }
 
-        Output::Log(Response.Key,Response.Tile,FailedInput);
+        // Output::Log(Key,Response.Tile,FailedInput);
+
+        switch (GameState) {
+            case States::Game::Playon: {
+                break;
+            }
+
+            case States::Game::Win: {
+                break;
+            }
+
+            case States::Game::Lose: {
+                break;
+            }
+        }
 
     } while (true);
 
